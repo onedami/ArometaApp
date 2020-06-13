8588d9e13fd34dbef08b4ea97f7c575ff3b7b502
Oludamilare Matthews <oludamilare.matthews@sage.com>
2020-06-13 13:11:50 +0100

    rearranged code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Ijoba  arometa leleyi");
-});
-
 const port = process.env.port || 3000;
 app.listen(port, () =>{
   console.log("Eyin arometa ekabo. An ri yin o!");
+});
+
+app.get('/', (req, res) => {
+  res.send("Ijoba  arometa leleyi");
 });
 
