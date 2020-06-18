484ab17551075f715938d6c3a03397056c0216fc
Oludamilare Matthews <oludamilare.matthews@sage.com>
2020-06-18 23:45:07 +0100

    Changed port number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8081;
 app.listen(port, () =>{
   console.log("Eyin arometa ekabo. An ri yin o!");
 });
